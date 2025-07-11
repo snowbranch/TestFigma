ca4303a5b5eefeca06a77a43c365b271cfc58658
snowbranch <272886473@qq.com>
2025-07-11 03:57:43 +0000

    11

--- src/client/ui/components/organisms/ItemSelection.story.tsx
+++ src/client/ui/components/organisms/ItemSelection.story.tsx
@@ -1,17 +1,22 @@
 import React from "@rbxts/react";
+import { createRoot } from "@rbxts/react-roblox";
 import { ItemSelection } from "./ItemSelection";
 
-export = () => {
-    return (
-        <screengui ResetOnSpawn={false}>
-            <ItemSelection
-                onIconSelect={(iconIndex) => {
-                    print(`选择了图标: ${iconIndex}`);
-                }}
-                onConfirm={(selectedIcon) => {
-                    print(`确认选择的图标: ${selectedIcon}`);
-                }}
-            />
-        </screengui>
+export = (target: GuiObject) => {
+    const root = createRoot(target);
+    
+    root.render(
+        <ItemSelection
+            onIconSelect={(iconIndex) => {
+                print(`选择了图标: ${iconIndex}`);
+            }}
+            onConfirm={(selectedIcon) => {
+                print(`确认选择的图标: ${selectedIcon}`);
+            }}
+        />
     );
+    
+    return () => {
+        root.unmount();
+    };
 };
